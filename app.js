dbe7bd0eb2b1ff756e432b5692d2cb48cd472f27
rmshahroz <rm.shahroz@gmail.com>
2024-08-17 10:29:58 +0500

    completed

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-var input = document.querySelector("#input")
-var p = document.querySelector ("#result")
 
 
+var img = document.querySelector('img')
+var masjid = document.querySelector ('#masjid')
+var chand = document.querySelector ('#chand')
+var p = document.querySelector ('p')
 
-function Coin(){
-if()
+function tossCoin (coin) {
+    
+    var randomNumber = Math.floor(Math.random()*2)
+    
+    if (randomNumber === 0 && coin === 'masjid') {
+        img.src = 'https://en.numista.com/catalogue/photos/pakistan/581-original.jpg'
+        p.innerHTML = 'You won the toss'
+    }else if (randomNumber === 0 && coin === 'chand') {
+        img.src = 'https://en.numista.com/catalogue/photos/pakistan/581-original.jpg'
+        p.innerHTML = 'You loss the toss'
+    }else if (randomNumber === 1 && coin === 'chand') {
+        img.src = 'https://en.numista.com/catalogue/photos/pakistan/582-original.jpg'
+        p.innerHTML = 'You won the toss'
+    }else if (randomNumber === 1 && coin === 'masjid') {
+        img.src = 'https://en.numista.com/catalogue/photos/pakistan/582-original.jpg'
+        p.innerHTML = 'You loss the toss'
+    }
 }
